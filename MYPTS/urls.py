d2aa6e1b4a355063858350f35b1f45bcef2a9d74
Safiullah Khan <safiullah.khan145@gmail.com>
2020-08-26 14:17:32 +0500

    2nd commit changes to left side bar

--- MYPTS/urls.py
+++ MYPTS/urls.py
@@ -36,7 +36,7 @@ urlpatterns = [
 
     # Users Views
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(template_name='users/home.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='dashboard/dashboard.html'), name='logout'),
     # path('profile/', user_views.profile, name='profile'),
     path('register/', user_views.register, name='register'),
     
